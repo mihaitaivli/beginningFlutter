6a11a749adb7756c3abb4cc282099f1390e8d4a6
Mihaita Ivli <mihaita.ivli@skybettingandgaming.com>
2021-04-03 11:15:36 +0100

    row

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,21 @@ class Home extends StatelessWidget {
         title: Text('Awesome App'),
         centerTitle: true,
       ),
-      body: Padding(
-        child: Text('hello world'),
-        padding: EdgeInsets.all(30.0),
+      body: Row(
+        children: <Widget>[
+          Text('hi there'),
+          TextButton(
+            onPressed: (){},
+            child: Text('click inside row child'),
+          ),
+          Container(
+            color: Colors.amber,
+            padding: EdgeInsets.all(30.0),
+            child: Text('some text inside a container'),
+          ),
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.end,
       ),
       floatingActionButton: FloatingActionButton(
         child: Text('Click'),
