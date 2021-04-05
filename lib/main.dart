696c141ec7301c76f32275119162c1ea3d304155
Mihaita Ivli <mihaita.ivli@skybettingandgaming.com>
2021-04-05 10:32:32 +0100

    add name fields

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,39 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text('Id card'),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0, // removes the shadow
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'NAME',
+              style: TextStyle(
+                // fontSize: 30.0,
+                color: Colors.grey,
+                letterSpacing: 1.5,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Alexandra Ivli',
+              style: TextStyle(
+                // fontSize: 30.0,
+                color: Colors.amberAccent[200],
+                letterSpacing: 1.5,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
