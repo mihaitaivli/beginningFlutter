07727b79744652689410446b07ae0ed86508201f
Mihaita Ivli <mihaita.ivli@skybettingandgaming.com>
2021-04-05 10:12:19 +0100

    add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,43 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  home: Home()
 ));
 
 class Home extends StatelessWidget {
-  Color defaultColor = Colors.blue[400];
-  Expanded createExpandedContainer(String content, Color color, int flexValue) => Expanded(
-    flex: flexValue,
-      child : Container(
-        padding: EdgeInsets.all(30.0),
-        color: color,
-        child: Text(content),
-      )
-  );
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Awesome App'),
+        title: Text('Id card'),
         centerTitle: true,
-        backgroundColor: defaultColor,
-      ),
-      body: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Image.asset('assets/stars.jpg')
-          ),
-          createExpandedContainer('1', Colors.cyan, 3),
-          createExpandedContainer('2', Colors.red, 2),
-          createExpandedContainer('3', Colors.green, 1),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Text('Click'),
-        onPressed: () => print('clicked'),
-        backgroundColor: defaultColor,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0, // removes the shadow
       ),
     );
   }
