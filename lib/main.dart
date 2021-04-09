d155152e88aae3f15fc143011effc8708dd789af
Mihaita Ivli <mihaita.ivli@skybettingandgaming.com>
2021-04-09 11:03:00 +0100

    implement dynamic list

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home()
+  home: QuoteList()
 ));
 
-class Home extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _HomeState extends State<Home> {
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'Flutter is great',
+    'Dart is amazing',
+    'Golang reigns supreme'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Quote List'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList()
+      ),
+    );
   }
 }
-
