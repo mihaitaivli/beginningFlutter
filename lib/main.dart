66762a8cc0d1aa11ac05304ab8f9b27359ae91e9
Mihaita Ivli <mihaitaivli@gmail.com>
2021-04-12 17:03:41 +0100

    extract quote card into separate component

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() => runApp(MaterialApp(
   home: QuoteList()
@@ -17,40 +18,6 @@ class _QuoteListState extends State<QuoteList> {
     Quote(text: 'Golang reigns supreme. Enough said.', author: 'me')
   ];
 
-  Widget quoteTemplate(Quote quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 20.0,
-                fontStyle: FontStyle.italic,
-                color: Colors.grey[600]
-              ),
-            ),
-            SizedBox(height: 6.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  quote.author,
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,8 +29,49 @@ class _QuoteListState extends State<QuoteList> {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: quotes.map((quote) => quoteTemplate(quote)).toList()
+        children: quotes.map((quote) => QuoteCard(quote: quote,)).toList()
       ),
     );
   }
 }
+
+// class QuoteCard extends StatelessWidget {
+//   final  Quote quote;
+//   QuoteCard({this.quote});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+//       child: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: [
+//             Text(
+//               quote.text,
+//               style: TextStyle(
+//                 fontSize: 20.0,
+//                 fontStyle: FontStyle.italic,
+//                 color: Colors.grey[600]
+//               ),
+//             ),
+//             SizedBox(height: 6.0),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.end,
+//               children: [
+//                 Text(
+//                   quote.author,
+//                   style: TextStyle(
+//                     fontSize: 14.0,
+//                     color: Colors.grey[500],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
