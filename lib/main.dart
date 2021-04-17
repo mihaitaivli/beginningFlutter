36e42e12b620c752464b0e236ffe528a5fe39aa2
Mihaita Ivli <mihaitaivli@gmail.com>
2021-04-17 16:05:57 +0100

    add initial routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:sl_fe/pages/choose_location.dart';
 import 'package:sl_fe/pages/home.dart';
+import 'package:sl_fe/pages/loading.dart';
 
-void main() => runApp(MaterialApp(
-    home: Home()
+void main() => runApp(
+  MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
   ),
 );
